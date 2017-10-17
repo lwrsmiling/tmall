e516b155fe27b9335af8557d59d7a2c215f8fb0d
lwrsmiling <wanru@ualberta.ca>
2017-10-17 14:18:57 -0700

    login logout functions added

--- js/config.js
+++ js/config.js
@@ -6,7 +6,12 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
     allowAnonymous: true
   })
     .when('/login', {
-    template: 'login please',
+    templateUrl: 'partials/login.html',
+    controller: 'loginCtrl',
+    allowAnonymous: true
+  })
+    .when('/logout', {
+    templateUrl: 'partials/home.html',
     allowAnonymous: true
   })
     .when('/cart', {
@@ -17,17 +22,47 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
     .otherwise({redirectTo: '/'});
 }]);
 
-app.run(function($location, $rootScope, $log, $route){
+app.run(function($location, $rootScope, $log, $route, $cookieStore, $http){
+  $rootScope.globals = $cookieStore.get('globals') || {};
+  if ($rootScope.globals.currentUser) {
+      $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+  }
   function onRouteChangeStart(event, next, current){
-    if(!next.allowAnonymous && !$rootScope.currentUser){
+    if(!next.allowAnonymous && !$rootScope.globals.currentUser){
       $log.log('Authentication required, redirect to login.');
-      var returnUrl = $location.url();
-      $log.log('return url is' + returnUrl);
+      var returnUrl = $location.path();
+      $log.log('return url is ' + returnUrl);
       event.preventDefault();
-      $location.path('/login').search({
-        returnUrl: returnUrl
-      });
+      $location.path('/login');
+//      $location.path('/login').search({
+//        returnUrl: returnUrl
+//      });
     }
   }
   $rootScope.$on('$routeChangeStart', onRouteChangeStart);
 });
+
+//app.run(['$rootScope', '$location', '$cookieStore', '$http', '$log',
+//    function ($rootScope, $location, $cookieStore, $http, $log) {
+//        // keep user logged in after page refresh
+//        $rootScope.globals = $cookieStore.get('globals') || {};
+//        if ($rootScope.globals.currentUser) {
+//            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+//        }
+//  
+//        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+//            // redirect to login page if not logged in
+////            if ($location.path() !== '/login' && !$rootScope.globals.currentUser) {
+////                $location.path('/');
+////            }
+//          if(!next.allowAnonymous && !$rootScope.globals.currentUser){
+//            $log.log('Authentication required, redirect to login.');
+//            var returnUrl = $location.url();
+//            $log.log('return url is' + returnUrl);
+//            event.preventDefault();
+//            $location.path('/login').search({
+//              returnUrl: returnUrl
+//            });
+//          }
+//        });
+//    }]);
---
+++ js/controllers.js
@@ -0,0 +1,102 @@
+app.controller('cateCtrl', function($scope, MyService){
+  $scope.categories = MyService.categories;
+  $scope.products = MyService.products;
+  $scope.loading = [false, false];
+  $scope.hovering = -1;
+  $scope.showProduct = function(index){
+    $scope.loading[index] = true;
+    $scope.hovering = index;
+  }
+  $scope.hideProduct = function(index){
+    $scope.loading[index] = false;
+    $scope.hovering = -1;
+    
+  }
+});
+
+app.controller('homeCateCtrl', function($scope, MyService){
+  $scope.cateProducts = MyService.getCateProducts();
+  $scope.addItem = function (parentIndex, childIndex){
+			MyService.addItem(parentIndex, childIndex);
+      alert(parentIndex, childIndex);
+		};
+});
+
+app.controller('cartCtrl', function($scope, MyService){
+  $scope.discount = MyService.discount;
+  $scope.items = MyService.getCartItems();
+  $scope.addQuantity = function(index){
+    MyService.addQuantity(index);
+  };
+  $scope.reduceQuantity = function(index){
+    MyService.reduceQuantity(index);
+  };
+  $scope.removeQuantity = function(index){
+    MyService.removeQuantity(index);
+  };
+  $scope.getQuantities = function(){
+    var totalNum = 0;
+    $scope.items.forEach(function(item, index){
+      totalNum += parseInt(item.quantity);
+    });
+    return totalNum;
+  };
+  $scope.getAmount = function(){
+    var amount = 0;
+    $scope.items.forEach(function(item, index){
+      amount += parseInt(item.price*item.quantity);
+    });
+    return amount;
+  };
+});
+
+app.controller('nav', function($scope, MyService, $rootScope, $cookieStore, AuthService){
+  $scope.items = MyService.getCartItems();
+  $scope.getQuantities = function(){
+    var totalNum = 0;
+    $scope.items.forEach(function(item, index){
+      totalNum += parseInt(item.quantity);
+    });
+    return totalNum;
+  };
+  $scope.logout = function(){
+    AuthService.clearCredentials();
+    $rootScope.isLoggedIn = false;
+//    $scope.isLoggedIn = false;
+  }
+});
+
+//app.controller('loginCtrl', function($scope, $rootScope, AUTH_EVENTS, AuthService){
+//  $scope.credentials = {
+//    username: '',
+//    password: ''
+//  };
+//  $scope.login = function(credentials){
+//    AuthService.login(credentials).then(function(user){
+//      $rootScope.$broadcast(AUTH_EVENTS.loginSuccess);
+//      $scope.setCurrentUser(user);
+//    }, function(){
+//      $rootScope.$broadcast(AUTH_EVENTS.loginFailed);
+//    });
+//  };
+//  javascript:void(0);
+//});
+app.controller('loginCtrl', function($scope, $rootScope, $location, AuthService){
+  AuthService.clearCredentials();
+  $scope.login = function(){
+    $scope.dataLoading = true;
+    AuthService.login($scope.username, $scope.password, function(response){
+      if(response.success){
+        $rootScope.isLoggedIn = true;
+        AuthService.setCredentials($scope.username, $scope.password);
+        console.log($location.path());
+        $location.path('/');
+      }
+      else{
+        $rootScope.isLoggedIn = false;
+        $scope.error = response.message;
+        $scope.dataLoading = false;
+      }
+    });
+  };
+});
